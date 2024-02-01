1464355cb62da17f831ec4e89f46ac101c882463
Stefano <snebo54@hotmail.com>
2024-02-01 18:53:33 +0100

    create player class to store player information

---
+++ lib/player.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+ 
+# Player class stores and manages player information
+class Player
+  attr_accessor :positions, :score, :name, :symbol
+
+  def initialize(name, symbol = Symbol.choose_sym, positions = [], score = 0 )
+    @name = name
+    @symbol = symbol
+    @positions = positions
+    @score = score
+  end
+end
+
+#creates a symbol (icon) for the player
+class Symbol
+  def choose_sym
+    #create an array of symbols and use rand to choose one of the array items
+    symbols = []
+    choice = symbols[rand (0..symbols.length-1)]
+    choice
+  end
+end
