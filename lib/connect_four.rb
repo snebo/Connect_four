8fd20a155c63a64b457c9f1260b4c6f35d4d96a5
Stefano <snebo54@hotmail.com>
2024-02-02 12:38:27 +0100

    add play round methods add methods to verify player inputs add methods to check horizontal and vertical game wins on each turn

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
-require_relative 'player'
+
+require_relative './player'
 
 # ConnectFour four constructor class
 class ConnectFour
-
   def initialize
     @board = Array.new(7) { Array.new(7) }
     @player1
     @player2
+    @turn = true
   end
 
   def draw_board(board = @board)
@@ -33,7 +34,7 @@ class ConnectFour
     puts line
     row_count.each_with_index do |_, idx|
       print row_count[idx]
-      board.each_with_index { |val2, idx2| board[idx][idx2].nil? ? print(' |  ') : print(' | x') }
+      board.each_with_index { |val2, idx2| board[idx][idx2].nil? ? print(' |  ') : print(" | #{board[idx][idx2]}") }
       print " |\n #{line}\n"
     end
     puts col_count
@@ -43,6 +44,112 @@ class ConnectFour
     puts 'Hi, welcome to Connect four, played in the termianl'
   end
 
+  def yes_or_no(question)
+    loop do
+      print question
+      response = gets.chomp.downcase
+      break if response == 'y' || response == 'n'
+
+      puts 'Not a valid response'
+    end
+    response == 'y' ? (return true) : (return false)
+  end
+
+  def create_players
+    # get player information
+    print "\nEnter your name player one: "
+    name1 = gets.chomp
+    setsym = yes_or_no("\nDo you have a custom symbol (y,n)")
+    setsym ? sym1 = gets.chomp : sym1 = Symbol.choose_sym
+
+    print "\nEnter your name player two: "
+    name2 = gets.chomp
+    setsym = yes_or_no("\nDo you have a custom symbol (y,n)")
+    setsym ? sym2 = gets.chomp : sym2 = Symbol.choose_sym
+
+    # set player info
+    @player1 = Player.new(name1, sym1)
+    @player2 = Player.new(name2, sym2)
+  end
+
+  def game_round
+    @turn = !@turn
+    draw_board(@board)
+    @turn ? play_turn(@player1) : play_turn(@player2)
+  end
+
+  def play_turn(player)
+    choice = valid_slot(player)
+    # should change the value into an array
+    # store that as player position and update board
+
+    # convert into array of numbers
+    choice = convert_choice(choice)
+    # update board
+    @board[choice[0]] [choice[1]] = player.symbol
+    # check win
+    check_win?
+  end
+
+  def check_win?
+    # check win dioganally, horizontally, vertically
+    new_arr = []
+    # horizontally
+    for i in 0..6
+      for j in 0..6
+        if new_arr.length >= 4
+          return true
+        end
+        if board[i][j] == board[i][j+1]
+          new_arr << board[i][j]
+        else
+          new_arr = []
+        end
+      end
+    end
+
+    # vertically
+    for i in 0..6
+      for j in 0..6
+        if new_arr.length >= 4
+          return true
+        end
+        if board[i][j] == board[i+1][j]
+          new_arr << board[i][j]
+        else
+          new_arr = []
+        end
+      end
+    end
+
+    return false
+  end
+
+  def convert_choice(value)
+    new_val = []
+    value = value.split('')
+    values = %w[a b c d e f]
+    # value = [values.index(value[0]), value[1]]
+    new_val << values.index(value[0])
+    new_val << value[1]
+    new_val
+  end
+
+  def valid_slot(player)
+    loop do
+      print "#{player.name} please choose a valid slot(a1, b2,...): "
+      choice = gets.chomp
+      choice = choice.split('')
+      if choice[0].match?(/[a-f]/) && choice[1].match?(/[1-7]/)
+        check = convert_choice(choice)
+        if @board[check[0]][check[1]].nil?
+          return choice
+          # break
+        end
+      end
+    end
+  end
+  
   def load_game
     list = Dir['save_files/*']
     list.each {|i| i.gsub!('.json', '').gsub!('save_files/', '')}
@@ -51,13 +158,15 @@ class ConnectFour
     print "\nEnter the name of your save: "
     save_file = gets.chomp
     file_path = "save_files/#{save_file}.json"
-    if FIle.exist?(file_path)
+    if File.exist?(file_path)
       save = File.read(file_path)
       info = JSON.phrase(save)
       @player1.name = info[]
       @player1.symbol = info[]
       @player1.score = info[]
       @player1.positions = inof[]
+
+      @turn = info[turn]
 
       puts "Welcom back #player_one and #player_two\n"
     end
--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
- 
+
 # Player class stores and manages player information
 class Player
   attr_accessor :positions, :score, :name, :symbol
@@ -12,12 +12,12 @@ class Player
   end
 end
 
-#creates a symbol (icon) for the player
+# creates a symbol (icon) for the player
 class Symbol
   def choose_sym
-    #create an array of symbols and use rand to choose one of the array items
-    symbols = []
-    choice = symbols[rand (0..symbols.length-1)]
-    choice
+    # create an array of symbols and use rand to choose one of the array items
+    symbols = %w[@ $ # & = + A X O M]
+    choice = symbols[rand(0..symbols.length-1)]
+    return choice
   end
 end
